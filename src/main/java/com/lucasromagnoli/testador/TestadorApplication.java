3ec53a4a5df0fd9c335a8f859dab667f73d419c8
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2019-10-10 21:33:50 -0300

    Implementando a interface CommandLineRunner para orquestrar os futuros testes

--- src/main/java/com/lucasromagnoli/testador/TestadorApplication.java
+++ src/main/java/com/lucasromagnoli/testador/TestadorApplication.java
@@ -1,13 +1,19 @@
 package com.lucasromagnoli.testador;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestadorApplication {
+public class TestadorApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestadorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
 	}
 
 }
